85234d20de19a34d676b33987ac9b14cee42e41a
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-02-20 04:18:53 +0530

    first commit

---
+++ src/component/MOS.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const MOS = () => {
+  return (
+    <div>
+        <h1>Mobile Operating System</h1>
+        <ul>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>iphone</li>
+            <li>Window Phone</li>
+        </ul>
+    </div>
+  )
+}
